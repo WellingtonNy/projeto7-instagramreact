573da379ca7dc8df5bc54f1ddd5a54168558e100
WellingtonLinux <wellingtoncpfilho@gmail.com>
2023-02-12 12:17:48 -0300

    add Sugestoes.map

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -3,19 +3,19 @@ export default function Stories() {
 
 
   const dadosStories = [
-    { imagemStories: "assets/img/9gag.svg", nomeStories: '9gag' },
-    { imagemStories: "assets/img/meowed.svg", nomeStories: "meowed" },
-    { imagemStories: "assets/img/barked.svg", nomeStories: "barked" },
-    { imagemStories: "assets/img/nathanwpylestrangeplanet.svg", nomeStories: "nathanwpylestrangeplanet" },
-    { imagemStories: "assets/img/wawawicomics.svg", nomeStories: "wawawicomics" },
-    { imagemStories: "assets/img/respondeai.svg", nomeStories: "respondeai" },
-    { imagemStories: "assets/img/filomoderna.svg", nomeStories: "filomoderna" },
-    { imagemStories: "assets/img/memeriagourmet.svg", nomeStories: "memeriagourmet" }]
+    { img: "assets/img/9gag.svg", name: '9gag' },
+    { img: "assets/img/meowed.svg", name: "meowed" },
+    { img: "assets/img/barked.svg", name: "barked" },
+    { img: "assets/img/nathanwpylestrangeplanet.svg", name: "nathanwpylestrangeplanet" },
+    { img: "assets/img/wawawicomics.svg", name: "wawawicomics" },
+    { img: "assets/img/respondeai.svg", name: "respondeai" },
+    { img: "assets/img/filomoderna.svg", name: "filomoderna" },
+    { img: "assets/img/memeriagourmet.svg", name: "memeriagourmet" }]
 
 
   return (
     <div class="stories">
-      {dadosStories.map((Sto) => <Story imagem={Sto.imagemStories} nome={Sto.nomeStories} />)}
+      {dadosStories.map((Sto) => <Story imagem={Sto.img} nome={Sto.name} />)}
       <div class="setinha">
         <ion-icon name="chevron-forward-circle"></ion-icon>
       </div>
--- src/components/Sugestoes.js
+++ src/components/Sugestoes.js
@@ -1,70 +1,19 @@
-export default function Sugestoes(){
-    return(
-        <div class="sugestoes">
-            <div class="titulo">
-              Sugestões para você
-              <div>Ver tudo</div>
-            </div>
-        
-            <div class="sugestao">
-              <div class="usuario">
-                <img src="assets/img/bad.vibes.memes.svg" alt="bad.vibes.memes.svg"/>
-                <div class="texto">
-                  <div class="nome">bad.vibes.memes</div>
-                  <div class="razao">Segue você</div>
-                </div>
-              </div>
-        
-              <div class="seguir">Seguir</div>
-            </div>
-        
-            <div class="sugestao">
-              <div class="usuario">
-                <img src="assets/img/chibirdart.svg" alt="chibirdart"/>
-                <div class="texto">
-                  <div class="nome">chibirdart</div>
-                  <div class="razao">Segue você</div>
-                </div>
-              </div>
-        
-              <div class="seguir">Seguir</div>
-            </div>
-        
-            <div class="sugestao">
-              <div class="usuario">
-                <img src="assets/img/razoesparaacreditar.svg" alt="razoesparaacreditar" />
-                <div class="texto">
-                  <div class="nome">razoesparaacreditar</div>
-                  <div class="razao">Novo no Instagram</div>
-                </div>
-              </div>
-        
-              <div class="seguir">Seguir</div>
-            </div>
-        
-            <div class="sugestao">
-              <div class="usuario">
-                <img src="assets/img/adorable_animals.svg" alt="adorable_animals"/>
-                <div class="texto">
-                  <div class="nome">adorable_animals</div>
-                  <div class="razao">Segue você</div>
-                </div>
-              </div>
-        
-              <div class="seguir">Seguir</div>
-            </div>
-        
-            <div class="sugestao">
-              <div class="usuario">
-                <img src="assets/img/smallcutecats.svg" alt="smallcutecats"/>
-                <div class="texto">
-                  <div class="nome">smallcutecats</div>
-                  <div class="razao">Segue você</div>
-                </div>
-              </div>
-        
-              <div class="seguir">Seguir</div>
-            </div>
-          </div>
-    )
+import Sugestao from "./Sugestao"
+export default function Sugestoes() {
+  const suges = [
+    { img: "assets/img/bad.vibes.memes.svg", nome: "bad.vibes.memes", razao: "Segue você" },
+    { img: "assets/img/chibirdart.svg", nome: "chibirdart", razao: "Segue você" },
+    { img: "assets/img/razoesparaacreditar.svg", nome: "razoesparaacreditar", razao: "Novo no Instagram" },
+    { img: "assets/img/adorable_animals.svg", nome: "adorable_animals", razao: "Segue você" },
+    { img: "assets/img/smallcutecats.svg", nome: "smallcutecats", razao: "Segue você" }
+  ]
+  return (
+    <div class="sugestoes">
+      <div class="titulo">
+        Sugestões para você
+        <div>Ver tudo</div>
+      </div>
+      {suges.map((sug) => <Sugestao img={sug.img} nome={sug.nome} razao={sug.razao} />)}
+    </div>
+  )
 }
